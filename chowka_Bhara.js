fc5787ee05e194c74d304d43bce8076f64215702
karthikeya <122973946+karthikeya9900@users.noreply.github.com>
2024-11-21 16:31:49 +0530

    Update chowka_Bhara.js

--- chowka_Bhara.js
+++ chowka_Bhara.js
@@ -1,2 +1,11 @@
-// it is 4 player game
-gfhgjhgjh
+function replaceAtIndex(targetIndex, string, char) {
+  let returnString = "";
+
+  for (let index = 0; index < string.length; index++) {
+    let currentChar = targetIndex === index ? char : string[index];
+
+    returnString += currentChar;
+  }
+
+  return returnString;
+}
